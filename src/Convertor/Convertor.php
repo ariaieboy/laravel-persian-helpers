70b2d7a85cc6f1d32472b332951d1b7aaac5a9c2
ariaieboy <ariaieboy@users.noreply.github.com>
2024-12-15 11:35:40 +0000

    Fix styling

--- src/Convertor/Convertor.php
+++ src/Convertor/Convertor.php
@@ -5,12 +5,12 @@ namespace Ariaieboy\LaravelPersianHelpers\Convertor;
 class Convertor
 {
     /**
-     * @param array<string,string> $rules
+     * @param  array<string,string>  $rules
      */
-    public function __construct(public array $rules = [])
+    public function __construct(public array $rules = []) {}
+
+    public function convert(string $input): string
     {
-    }
-    public function convert(string $input): string{
         return strtr($input, $this->rules);
     }
 }
--- tests/Convertor/ConvertorTest.php
+++ tests/Convertor/ConvertorTest.php
@@ -2,10 +2,10 @@
 
 use Ariaieboy\LaravelPersianHelpers\Convertor\Convertor;
 
-it('can convert strings using custom rules',function (){
+it('can convert strings using custom rules', function () {
     $rules = [
-        '1'=>'۱',
-        '2'=>'۲'
+        '1' => '۱',
+        '2' => '۲',
     ];
     $convertor = new Convertor($rules);
     expect($convertor->convert('1221'))->toBe('۱۲۲۱');
