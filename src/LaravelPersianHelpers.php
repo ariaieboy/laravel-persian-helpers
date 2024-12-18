1ccb822c1dcef8f685adb4774e242fba9d2b9b33
AriaieBOY <ariaieboy.ir@gmail.com>
2024-12-18 18:11:02 +0330

    refactor

--- src/LaravelPersianHelpers.php
+++ src/LaravelPersianHelpers.php
@@ -8,21 +8,20 @@ class LaravelPersianHelpers
 {
     public function toEnglishDigit(string $string, ?array $customRules = null): string
     {
-        $rules = ! blank($customRules) ? $customRules : config('persian-helpers.to_english_digit');
-
+        $rules = !blank($customRules) ? $customRules : config('persian-helpers.rules.to_english_digit');
         return new Convertor($rules)->convert($string);
     }
 
     public function toPersianDigit(string $string, ?array $customRules = null): string
     {
-        $rules = ! blank($customRules) ? $customRules : config('persian-helpers.to_persian_digit');
+        $rules = !blank($customRules) ? $customRules : config('persian-helpers.rules.to_persian_digit');
 
         return new Convertor($rules)->convert($string);
     }
 
     public function toPersianLetter(string $string, ?array $customRules = null): string
     {
-        $rules = ! blank($customRules) ? $customRules : config('persian-helpers.to_persian_letter');
+        $rules = !blank($customRules) ? $customRules : config('persian-helpers.rules.to_persian_letter');
 
         return new Convertor($rules)->convert($string);
     }
@@ -30,7 +29,7 @@ class LaravelPersianHelpers
     public function toPersian(string $string, ?array $customRules = null): string
     {
 
-        $rules = ! blank($customRules) ? $customRules : array_merge(config('persian-helpers.to_persian_digit'), config('persian-helpers.to_persian_letter'));
+        $rules = !blank($customRules) ? $customRules : array_merge(config('persian-helpers.rules.to_persian_digit'), config('persian-helpers.rules.to_persian_letter'));
 
         return new Convertor($rules)->convert($string);
 
